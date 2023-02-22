5e098726419da8db04942df27ba2e2ca49666e96
Koprada <Koprada@javeriana.edu.co>
2023-02-22 18:24:59 -0500

    archivo

--- taller2_2_analisisalgortimos.py
+++ taller2_2_analisisalgortimos.py
@@ -7,10 +7,8 @@ import numpy as np
 
 
 #Creacion de archivo que incrementa de 100 en 100 datos hasta llegar a 30000
-def writeData(pos):
-    max_n = 100000
+def escritura_archivo(pos):
     max_num = 12
-    ns = set()
 
     with open("input.in", "a") as f:
         numbers = []
@@ -21,33 +19,36 @@ def writeData(pos):
         f.write("\n")
 
 for i in range(300):
-  writeData(i + 1)
+  escritura_archivo(i + 1)
+
 
 #Algoritmo 
-def max_idiferencia_dividir_y_vencer(S, b, e):
+def max_idiferencia_dividir_y_vencer(s, b, e):
     if  b - e < 2:
         return 0
     q= (b + e) // 2
 
-    suma_izquierda = sum(S[b:q])
-    suma_derecha = sum(S[q:e])
+    suma_izquierda = sum(s[b:q])
+    suma_derecha = sum(s[q:e])
 
     diferencia1 = suma_izquierda - suma_derecha
-    diferencia2 = max_idiferencia_dividir_y_vencer(S, b, q)
-    diferencia3 = max_idiferencia_dividir_y_vencer(S, q, e)
+    diferencia2 = max_idiferencia_dividir_y_vencer(s, b, q)
+    diferencia3 = max_idiferencia_dividir_y_vencer(s, q, e)
     max_diferencia = max(abs(diferencia1), abs(diferencia2), abs(diferencia3))
 
     return max_diferencia
 
 
 
-#Apertura del archivo.
-def MeasureTime(S):
-  sT = tiempo.process_time()
-  max_idiferencia_dividir_y_vencer(S,0,len(S))
-  eT = tiempo.process_time()
-  return (eT - sT)
+#Captura de tiempos
+def measure_time(s):
+  st = tiempo.process_time()
+  max_idiferencia_dividir_y_vencer(s,0,len(s))
+  et = tiempo.process_time()
+  return (et - st)
   
+
+
 # Abrir el archivo .in en modo lectura
 with open('input.in', 'r') as f:
     # Leer cada línea del archivo
@@ -60,12 +61,12 @@ for linea in lineas:
     # Eliminar los espacios en blanco al inicio y al final de la línea
     linea = linea.strip()
     # Separar los valores por espacios en blanco y convertirlos a números
-    S = [float(x) for x in linea.split()]
+    s = [float(x) for x in linea.split()]
     # Agregar los valores a la lista de números
-    time = MeasureTime(S)
-    print(len(S),f"{time: .15f}")
+    time = measure_time(s)
+    print(len(s),f"{time: .15f}")
     with open("output.res","a") as file:
-      file.write(f"{len(S)} {time:.15f}\n")
+      file.write(f"{len(s)} {time:.15f}\n")
 
 
 
